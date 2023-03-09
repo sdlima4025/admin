80ed689e472257181d6a37732a4e731b53cd7364
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-09 11:24:07 -0300

    atualizado requisição com fetch API

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -18,28 +18,28 @@ linhaNovoCliente.innerHTML = conteudo
 const tabela =  document.querySelector('[data-tabela]')
 
 const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-        http.open('GET', 'http://localhost:3000/profile')
-        
-        http.onload = () => {
-           if(http.status >= 400) {
-                reject(JSON.parse(http.response)) 
-                }else {
-                    resolve(JSON.parse(http.response))
-                }
-           }
-           http.send()
-        })
-    return promise
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json()
+    })
 }
 
 listaClientes()
 .then( data => {
        data.forEach(elemento => {
        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-        })
-})
+        })})
 
 
 // fetch API ?
+
+
+
+// Nesta aula, aprendemos:
+
+//     Fazer requisições utilizando XMLHttpRequest.
+//     Lidar com promises.
+//     Fazer uma requisição http utilizando XMLHttpRequest para buscar todos os clientes do servidor.
+//     Refatorar o código utilizando promises melhorando a leitura do código.
+//     Utilizar template literals para criar um template html.
+
