dee84cf2aee9822319a77db30a11221537167a8d
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-12 20:31:12 -0300

    atualizado a função novo cliente

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,10 +1,31 @@
 const listaClientes = () => {
-    return fetch(`http://localhost:3000/profile`)
+    return fetch(`http://localhost:3000/profile`) // fetch com GET
     .then(resposta => {
         return resposta.json()
     })
 }
 
+// FORMULARIO NOVO CLIENTE - POST
+
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST', 
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify ({
+            nome: nome,
+            email: email
+        })
+        
+        })
+        .then(resposta => {
+            return resposta.body
+    })
+}
+
+// Objeto Service
 export const clienteService = {
-    listaClientes
+    listaClientes,
+    criaCliente
 }
