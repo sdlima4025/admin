1d8e1d78c6639c6e660626b04fb691688673a9a3
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-13 17:27:28 -0300

    substituindo o .then para async await para maior legibilidade

--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -20,23 +20,21 @@ const criaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector('[data-tabela]')
 
-tabela.addEventListener('click', (evento) => {
+tabela.addEventListener('click', async (evento) => {
     let ehBotaoDeDeleta = evento.target.className === 'botao-simples botao-simples--excluir'
     if (ehBotaoDeDeleta) {
         const linhaCliente = evento.target.closest('[data-id]')
         let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-            .then(() => {
-                linhaCliente.remove()
-            })
-
+        await clienteService.removeCliente(id)
+            linhaCliente.remove()
     }
 })
 
-
-clienteService.listaClientes()
-    .then(data => {
-        data.forEach(elemento => {
+const render = async() => {
+    const listaClientes = await clienteService.listaClientes()
+    
+    listaClientes.forEach(elemento => {
             tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
-        })
     })
+}
+render()
